4e2b4516fee4d25f1ed7f4f0aa609c1fffa5b26f
Pappu <989572@corpaa.aa.com>
2016-07-21 23:22:36 -0500

    fixed bullets

--- src/main/java/com/aa/ndchtml5/common/Feature.java
+++ src/main/java/com/aa/ndchtml5/common/Feature.java
@@ -4,8 +4,8 @@ public class Feature extends DynamicView {
 
 	public static Feature SEAT_BACK_ENTERTAINMENT = new Feature("Seat Back Entertainment", "/images/seat_back_entertainment.jpg");
 	public static Feature POWER_PORTS = new Feature("PowerPorts", "/images/power_ports.jpg");
-	public static Feature MAIN_CABIN_SEAT = new Feature("Main Cabin Seat", "/images/main_ cabin_ seat.jpg");
-	public static Feature PRE_ORDERED_MEALS = new Feature("Pre Ordered Meals", "/images/pre_ ordered_meals.jpg");
+	public static Feature MAIN_CABIN_SEAT = new Feature("Main Cabin Seat", "/images/main_cabin_seat.jpg");
+	public static Feature PRE_ORDERED_MEALS = new Feature("Pre Ordered Meals", "/images/pre_ordered_meals.jpg");
 	public static Feature WIFI = new Feature("Wifi", "/images/wifi.jpg");
 	
 	public static final Feature[] featuresList = {SEAT_BACK_ENTERTAINMENT,POWER_PORTS,MAIN_CABIN_SEAT,PRE_ORDERED_MEALS,WIFI};
