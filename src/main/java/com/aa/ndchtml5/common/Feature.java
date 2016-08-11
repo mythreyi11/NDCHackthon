3ab63668f6b50e114b3ea017cf600097e162134d
Pappu <989572@corpaa.aa.com>
2016-08-11 14:43:36 -0500

    added more features to filters

--- src/main/java/com/aa/ndchtml5/common/Feature.java
+++ src/main/java/com/aa/ndchtml5/common/Feature.java
@@ -7,8 +7,11 @@ public class Feature extends DynamicView {
 	public static Feature MAIN_CABIN_SEAT = new Feature("Main Cabin Seat", "/images/main_cabin_seat.jpg");
 	public static Feature PRE_ORDERED_MEALS = new Feature("Pre Ordered Meals", "/images/pre_ordered_meals.jpg");
 	public static Feature WIFI = new Feature("Wifi", "/images/wifi.jpg");
+	public static Feature ADVANCED_SEAT_SELECTION = new Feature("Advanced Seat Selection", "/images/advanced_seat_selection.jpg");
+	public static Feature GROUP_ONE_BOARDING = new Feature("Group one boarding", "/images/group_one_boarding.jpg");
 	
-	public static final Feature[] featuresList = {SEAT_BACK_ENTERTAINMENT,POWER_PORTS,MAIN_CABIN_SEAT,PRE_ORDERED_MEALS,WIFI};
+	
+	public static final Feature[] featuresList = {SEAT_BACK_ENTERTAINMENT,POWER_PORTS,MAIN_CABIN_SEAT,PRE_ORDERED_MEALS,WIFI,ADVANCED_SEAT_SELECTION, GROUP_ONE_BOARDING};
 	
 	public Feature(String code, String value) {
 		super(code, value);
